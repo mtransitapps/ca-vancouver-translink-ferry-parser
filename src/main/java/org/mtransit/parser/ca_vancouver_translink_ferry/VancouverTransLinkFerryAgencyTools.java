2fcfb147ef0cb61ea12a99606428a0d969ed6885
Mathieu Méa <mathieu.mea@gmail.com>
2021-09-28 10:45:45 -0400

    Cleanup

--- src/main/java/org/mtransit/parser/ca_vancouver_translink_ferry/VancouverTransLinkFerryAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_vancouver_translink_ferry/VancouverTransLinkFerryAgencyTools.java
@@ -1,29 +1,19 @@
 package org.mtransit.parser.ca_vancouver_translink_ferry;
 
+import static org.mtransit.commons.Constants.SPACE_;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.mtransit.parser.CleanUtils;
+import org.mtransit.commons.CleanUtils;
 import org.mtransit.parser.DefaultAgencyTools;
 import org.mtransit.parser.MTLog;
-import org.mtransit.parser.StringUtils;
-import org.mtransit.parser.Utils;
-import org.mtransit.parser.gtfs.data.GCalendar;
-import org.mtransit.parser.gtfs.data.GCalendarDate;
 import org.mtransit.parser.gtfs.data.GRoute;
-import org.mtransit.parser.gtfs.data.GSpec;
 import org.mtransit.parser.gtfs.data.GStop;
-import org.mtransit.parser.gtfs.data.GTrip;
 import org.mtransit.parser.mt.data.MAgency;
-import org.mtransit.parser.mt.data.MRoute;
-import org.mtransit.parser.mt.data.MTrip;
 
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.regex.Pattern;
-
-import static org.mtransit.parser.Constants.SPACE_;
 
 // http://www.translink.ca/en/Schedules-and-Maps/Developer-Resources.aspx
 // http://www.translink.ca/en/Schedules-and-Maps/Developer-Resources/GTFS-Data.aspx
@@ -33,72 +23,25 @@ import static org.mtransit.parser.Constants.SPACE_;
 // http://gtfs.translink.ca/static/latest
 public class VancouverTransLinkFerryAgencyTools extends DefaultAgencyTools {
 
-	public static void main(@Nullable String[] args) {
-		if (args == null || args.length == 0) {
-			args = new String[3];
-			args[0] = "input/gtfs.zip";
-			args[1] = "../../mtransitapps/ca-vancouver-translink-ferry-android/res/raw/";
-			args[2] = ""; // files-prefix
-		}
+	public static void main(@NotNull String[] args) {
 		new VancouverTransLinkFerryAgencyTools().start(args);
 	}
 
 	@Nullable
-	private HashSet<Integer> serviceIdInts;
-
 	@Override
-	public void start(@NotNull String[] args) {
-		MTLog.log("Generating TransLink ferry data...");
-		long start = System.currentTimeMillis();
-		this.serviceIdInts = extractUsefulServiceIdInts(args, this, true);
-		super.start(args);
-		MTLog.log("Generating TransLink ferry data... DONE in %s.", Utils.getPrettyDuration(System.currentTimeMillis() - start));
+	public List<Locale> getSupportedLanguages() {
+		return LANG_EN;
 	}
 
 	@Override
-	public boolean excludingAll() {
-		return this.serviceIdInts != null && this.serviceIdInts.isEmpty();
+	public boolean defaultExcludeEnabled() {
+		return true;
 	}
 
+	@NotNull
 	@Override
-	public boolean excludeCalendar(@NotNull GCalendar gCalendar) {
-		if (this.serviceIdInts != null) {
-			return excludeUselessCalendarInt(gCalendar, this.serviceIdInts);
-		}
-		return super.excludeCalendar(gCalendar);
-	}
-
-	@Override
-	public boolean excludeCalendarDate(@NotNull GCalendarDate gCalendarDates) {
-		if (this.serviceIdInts != null) {
-			return excludeUselessCalendarDateInt(gCalendarDates, this.serviceIdInts);
-		}
-		return super.excludeCalendarDate(gCalendarDates);
-	}
-
-	// private static final long RID_SEA_BUS = 998L;
-	private static final List<String> INCLUDE_RSN = Arrays.asList("998", "SeaBus", "SEABUS");
-
-	private boolean isSeaBusRoute(GRoute gRoute) {
-		return INCLUDE_RSN.contains(gRoute.getRouteShortName()) //
-				|| INCLUDE_RSN.contains(gRoute.getRouteLongName());
-	}
-
-	@Override
-	public boolean excludeRoute(@NotNull GRoute gRoute) {
-		//noinspection RedundantIfStatement
-		if (!isSeaBusRoute(gRoute)) {
-			return true; // exclude
-		}
-		return false; // keep
-	}
-
-	@Override
-	public boolean excludeTrip(@NotNull GTrip gTrip) {
-		if (this.serviceIdInts != null) {
-			return excludeUselessTripInt(gTrip, this.serviceIdInts);
-		}
-		return super.excludeTrip(gTrip);
+	public String getAgencyName() {
+		return "TransLink";
 	}
 
 	@NotNull
@@ -108,29 +51,32 @@ public class VancouverTransLinkFerryAgencyTools extends DefaultAgencyTools {
 	}
 
 	@Override
-	public long getRouteId(@NotNull GRoute gRoute) {
-		return super.getRouteId(gRoute); // useful to match with GTFS real-time
+	public boolean defaultRouteIdEnabled() {
+		return true;
 	}
 
-	private static final String SEA_BUS_SHORT_NAME = "SB";
-	private static final String SEA_BUS_LONG_NAME = "SeaBus";
-
-	@Nullable
 	@Override
-	public String getRouteShortName(@NotNull GRoute gRoute) {
-		if (isSeaBusRoute(gRoute)) {
-			return SEA_BUS_SHORT_NAME;
-		}
-		throw new MTLog.Fatal("Unexpected route short name for %s!", gRoute);
+	public boolean useRouteShortNameForRouteId() {
+		return false; // useful to match with GTFS real-time
 	}
 
 	@NotNull
 	@Override
-	public String getRouteLongName(@NotNull GRoute gRoute) {
-		if (isSeaBusRoute(gRoute)) {
-			return SEA_BUS_LONG_NAME;
+	public String provideMissingRouteShortName(@NotNull GRoute gRoute) {
+		if (gRoute.getRouteLongNameOrDefault().equalsIgnoreCase("SeaBus")) {
+			return "SB";
 		}
-		throw new MTLog.Fatal("Unexpected route long name for %s!", gRoute);
+		throw new MTLog.Fatal("Unexpected route short name for %s!", gRoute);
+	}
+
+	@Override
+	public boolean defaultRouteLongNameEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean defaultAgencyColorEnabled() {
+		return true;
 	}
 
 	private static final String AGENCY_COLOR_BLUE = "0761A5"; // BLUE (merge)
@@ -147,32 +93,16 @@ public class VancouverTransLinkFerryAgencyTools extends DefaultAgencyTools {
 
 	@Nullable
 	@Override
-	public String getRouteColor(@NotNull GRoute gRoute) {
-		if (StringUtils.isEmpty(gRoute.getRouteColor())) {
-			if (isSeaBusRoute(gRoute)) {
-				return SEABUS_COLOR;
-			}
-			throw new MTLog.Fatal("Unexpected route color " + gRoute);
+	public String provideMissingRouteColor(@NotNull GRoute gRoute) {
+		if (gRoute.getRouteLongNameOrDefault().equalsIgnoreCase("SeaBus")) {
+			return SEABUS_COLOR;
 		}
-		return super.getRouteColor(gRoute);
-	}
-
-	@Override
-	public void setTripHeadsign(@NotNull MRoute mRoute, @NotNull MTrip mTrip, @NotNull GTrip gTrip, @NotNull GSpec gtfs) {
-		mTrip.setHeadsignString(
-				cleanTripHeadsign(gTrip.getTripHeadsignOrDefault()),
-				gTrip.getDirectionIdOrDefault()
-		);
+		throw new MTLog.Fatal("Unexpected route color " + gRoute);
 	}
 
 	@Override
 	public boolean directionFinderEnabled() {
 		return true;
-	}
-
-	@Override
-	public boolean mergeHeadsign(@NotNull MTrip mTrip, @NotNull MTrip mTripToMerge) {
-		throw new MTLog.Fatal("Unexpected trips to merge %s & %s!", mTrip, mTripToMerge);
 	}
 
 	@NotNull
